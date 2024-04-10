2f2e271aaf166f9023f3168c446fd3545a73290e
Amin Kiani <100538655+M-Amin-Kiani@users.noreply.github.com>
2024-04-10 21:47:17 +0330

    Add files via upload

---
+++ Frontend/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+const Home = () => {
+  return(
+      <div className={'h-screen w-screen bg-white'}>
+        <Header/>
+      </div>
+  )
+}
+export default Home;
