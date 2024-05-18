7edec999c790266c54eb691a4eab53da78c29ba0
Amin Kiani <100538655+M-Amin-Kiani@users.noreply.github.com>
2024-05-19 02:01:16 +0330

    Delete Frontend directory

--- Frontend/utils/api/login.js
+++
@@ -1,7 +0,0 @@
-import { apiService } from "./axios";
-
-export const loginAPI = async (data) => {
-  const response = await apiService.post("/users/professor-login/", data);
-
-  return response.data;
-};
