15e9667c1884187c64e3e31afc210e3ce66d0389
MJ202218 <mohamamdjafari18132@gmail.com>
2024-05-18 22:26:30 +0330

    cleaning backend branch

--- frontend/utils/falsyString.js
+++
@@ -1,6 +0,0 @@
-export const falsyString = (value) => {
-    if (typeof value === 'number') return value;
-    if (typeof value === 'string' && value.trim() !== '') return value;
-  
-    return '-';
-  };
