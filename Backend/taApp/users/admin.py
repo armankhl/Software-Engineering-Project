ac1fb6404fdf600a7927d6a6535869f7276a384e
Amin Kiani <100538655+M-Amin-Kiani@users.noreply.github.com>
2024-05-19 01:57:52 +0330

    Delete Backend/taApp directory

--- Backend/taApp/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Course)
-admin.site.register(models.StudentProfile)
-admin.site.register(models.ProfessorProfile)
-admin.site.register(models.Requests)
