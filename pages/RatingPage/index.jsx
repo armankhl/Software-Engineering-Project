f692451624f5616dc131f07d4d19b3085c4b2a9a
Yazdan Afra <yazdan9093@gmail.com>
2024-05-20 22:05:50 +0330

    Rating page

---
+++ pages/RatingPage/index.jsx
@@ -0,0 +1,16 @@
+import Layout from '@/components/layout';
+import RatingForm from '@/components/rating';
+import React from 'react'
+
+const RatingPage = () => {
+  return (
+    <>
+
+    <Layout> <RatingForm></RatingForm></Layout>
+   
+      
+    </>
+  )
+}
+
+export default RatingPage;
