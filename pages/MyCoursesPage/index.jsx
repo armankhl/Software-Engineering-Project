38379a711b47e6526bcfbd8bd4a55a93e6313bf6
Yazdan Afra <yazdan9093@gmail.com>
2024-05-20 22:07:28 +0330

    footer pages fixes

---
+++ pages/MyCoursesPage/index.jsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout";
+import MyCourses from "@/components/my-courses";
+
+
+const MyCoursesPage = () => {
+    return ( 
+   <Layout>
+    <MyCourses></MyCourses>
+   </Layout> 
+    );
+};
+
+export default MyCoursesPage;
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 export default function App({ Component, pageProps }) {
   const [client] = useState(
     new QueryClient({
